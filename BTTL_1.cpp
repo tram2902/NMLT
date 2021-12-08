c3c3dd3c57b8e5885a4fae2686acabf770bd7a45
tram2902 <86964729+tram2902@users.noreply.github.com>
2021-12-08 19:09:19 +0700

    Update BTTL_1.cpp

--- BTTL_1.cpp
+++ BTTL_1.cpp
@@ -1,10 +1,9 @@
-//#include<stdio.h>
-//#include<conio.h>
-//void main()
-//{
-//    float x;
-//    printf("\nNhap vao 1 so thuc : ");
-//    scanf("%f", &x);
-//    printf("\n %f Co phan nguyen cua la %d ", x, int(x));
-//    getch();
-//}
+#include<stdio.h>
+#include<conio.h>
+void main()
+{     float x;
+     printf("\nNhap vao 1 so thuc : ");
+     scanf("%f", &x);
+   printf("\n %f Co phan nguyen cua la %d ", x, int(x));
+  getch();
+}

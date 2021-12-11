3bbe275c10910f3762c83910eb5709dc47236124
tram2902 <86964729+tram2902@users.noreply.github.com>
2021-12-11 16:25:45 +0700

    Add files via upload

---
+++ Source2.cpp
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    {
+        int tgdenRL[10], tgxl[10], tam[10];
+        int i, nhonhat, dem = 0, thoigian, soTT;
+        double tgcho = 0, tght = 0, ketthuc;
+        float tgchotb, tghttb;
+        printf("\nNhap so tien trinh:\t");
+        scanf("%d", &soTT);
+        printf("\nNhap du lieu chi tiet cho %d tien trinh\n", soTT);
+        for (i = 0; i < soTT; i++)
+        {
+            printf("\nNhap thoi gian den hang doi Ready cua tien trinh thu %d :", i + 1);
+            scanf("%d", &tgdenRL[i]);
+            printf("Nhap thoi gian xu ly cua tien trinh thu %d :", i + 1);
+            scanf("%d", &tgxl[i]);
+            tam[i] = tgxl[i];
+        }
+        tgxl[9] = 60;  //Gia su thoi gian xu ly cua tien trinh cuoi cung la 60 giay
+
+        for (thoigian = 0; dem != soTT; thoigian++)
+        {
+            nhonhat = 9; //Xet tien trinh co thoi gian nho nhat (tien trinh sau cung)
+            for (i = 0; i < soTT; i++)
+            {
+                if (tgdenRL[i] <= thoigian && tgxl[i] < tgxl[nhonhat] && tgxl[i] > 0)
+                {
+                    nhonhat = i;
+                }
+            }
+            tgxl[nhonhat]--;
+            if (tgxl[nhonhat] == 0)
+            {
+                dem++;
+                ketthuc = thoigian + 1;
+                tgcho = tgcho + ketthuc - tgdenRL[nhonhat] - tam[nhonhat];
+                tght = tght + ketthuc - tgdenRL[nhonhat];
+            }
+        }
+        printf("\n\nTien trinh\tTG den ReadyList\tTG xuly\t\tTGcho\t\tTGhoantat\n");
+        for (i = 0; i < soTT; i++)
+        {
+            printf("\n%d\t\t%d\t\t\t%d\t\t%f\t%f", i + 1, tgdenRL[i], tgxl[i], tgcho, tght);
+        }
+        printf("\n");
+    }
+    
+
+ 
+    return 0;
+}
